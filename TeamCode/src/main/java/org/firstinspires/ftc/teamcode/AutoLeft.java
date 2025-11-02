c264fad9c4b1eeb42eb5ac3683fb15d890b53be5
arcpolaris <143139215+arcpolaris@users.noreply.github.com>
2025-11-02 14:17:08 -0800

    added simple autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeft.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.lib.Hardware;
+
+@Autonomous(name = "AutoLeft", group = "9884")
+public class AutoLeft extends LinearOpMode{
+    Hardware robot = new Hardware();
+
+    @Override
+    public void runOpMode(){
+        telemetry = robot.init(hardwareMap, telemetry);
+
+        waitForStart();
+
+        robot.moveTime(Hardware.VStrafe, -Hardware.AUTO_CONSTANT, 1000);
+        robot.moveTime(Hardware.VDrive, Hardware.AUTO_CONSTANT, 10_000);
+    }
+}
